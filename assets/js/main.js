6f603307c34f0b9ff30fe34ed37217f93dab1b1b
MaksimBorovik <borovik.maksim@gmail.com>
2019-09-29 23:46:28 +0300

    fixed the slider dots

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $('.header__slider-items').slick({
     adaptiveHeigh: true,
-    // autoplay: true,
+    autoplay: true,
     dots: true,
     appendDots: $('.header__slider'),
     arrows: false
