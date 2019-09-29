7c92260f8069b673d1d1f0dc592e4e7f5b62a10a
MaksimBorovik <borovik.maksim@gmail.com>
2019-09-29 23:29:51 +0300

    the header is done

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $('.header__slider-items').slick({
+    adaptiveHeigh: true,
+    // autoplay: true,
+    dots: true,
+    appendDots: $('.header__slider'),
+    arrows: false
+  });
+});
