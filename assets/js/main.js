de9f80d2c8eecd57ffb1331384af3bd40cd386ae
MaksimBorovik <borovik.maksim@gmail.com>
2019-10-16 21:41:14 +0300

    added the javaScript code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $('.header__slider-items').slick({
     adaptiveHeigh: true,
-    // autoplay: true,
+    autoplay: true,
     dots: true,
     appendDots: $('.header__slider'),
     arrows: false
@@ -9,3 +9,32 @@ $(document).ready(function(){
 });
 
 $(document).foundation();
+
+$('.header__top-controls').click(function() {
+  $('.header__top-button').toggleClass('active');
+});
+
+$('.header__slider-item-buttons').click(function() {
+  $('.header__slider-item-button').toggleClass('active');
+});
+
+$('.chart__menu-right-list').click(function() {
+  $('.chart__menu-right-list-item.chart__menu-right-list-item-link').toggleClass('active');
+});
+
+$('[data-js="open-full-info"]').on('click', function () {
+
+  let fullInfo = $(this).parents('.card').find('.chart__list-item-full-info').fadeToggle('fast');
+
+  let cardSection = $(this).parents('.card').find('.card-section').css({'display': 'none'});
+
+  let info = $(this).parents('.card').find('.chart__list-item-info').css({'display': 'none'});
+
+  return false;
+});
+
+$('[data-close]').on('click', function () {
+  let cardSection = $(this).parents('.card').find('.card-section').css({'display': 'flex'});
+
+  let info = $(this).parents('.card').find('.chart__list-item-info').css({'display': 'flex'});
+});
